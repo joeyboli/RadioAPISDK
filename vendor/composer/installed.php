ada0fc114676f5f05ddfe6c8b0d62c5dd93b07ff
Joey 😎 <43940776+joeyboli@users.noreply.github.com>
2025-10-12 21:55:30 +0000

    Bump package version to 1.0.1
    Update composer.json and related files to reflect new package version 1.0.1.

--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -1,8 +1,8 @@
 <?php return array(
     'root' => array(
         'name' => 'joeyboli/radioapi-php',
-        'pretty_version' => '1.0.0',
-        'version' => '1.0.0.0',
+        'pretty_version' => '1.0.1',
+        'version' => '1.0.1.0',
         'reference' => null,
         'type' => 'library',
         'install_path' => __DIR__ . '/../../',
@@ -20,8 +20,8 @@
             'dev_requirement' => false,
         ),
         'joeyboli/radioapi-php' => array(
-            'pretty_version' => '1.0.0',
-            'version' => '1.0.0.0',
+            'pretty_version' => '1.0.1',
+            'version' => '1.0.1.0',
             'reference' => null,
             'type' => 'library',
             'install_path' => __DIR__ . '/../../',
